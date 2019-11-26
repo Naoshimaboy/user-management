0dc3a1f53cd7cc6c1ec1f5d80cf06b06ffe76584
rodriguez nils <rdn2513a@u1208-07l.ens.fsi>
2019-11-26 08:38:12 +0100

    fin TP

--- src/test/java/geco/UserTest.java
+++ src/test/java/geco/UserTest.java
@@ -6,10 +6,12 @@ import static org.junit.Assert.assertNotEquals;
 public class UserTest {
 
     @Test
-    public void testAl(){
+    public void testAl() {
         User test = new User();
-        String toReturn = test.getRandomPassword();
-        assertNotEquals("azertyui", toReturn);
+        for (int i = 0; i < 10; i++) {
+            String toReturn = test.getRandomPassword();
+            assertNotEquals("azertyui", toReturn);
+        }
     }
 
 }
